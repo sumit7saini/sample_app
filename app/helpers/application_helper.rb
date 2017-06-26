f38e601f15d3b1dc02aa91c98eb0ee51f00ed9da
Sumit Saini <sainisumit123@gmail.com>
2017-06-26 18:19:28 +0530

    Add a full_title helperV.III

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 module ApplicationHelper
 
-<<<<<<< HEAD
 	
-end
-=======
+
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -14,4 +12,3 @@ end
     end
   end
 end
->>>>>>> rails-flavored-ruby
